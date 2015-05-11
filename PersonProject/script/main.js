d735e1b4ab98929a8426a148096d09d210e6ea8f
Muhammad Sibghat <sibghatuk@gmail.com>
2015-05-11 08:40:46 -0500

    added the logic for service

--- PersonProject/script/main.js
+++ PersonProject/script/main.js
@@ -1,20 +1,95 @@
 var app = angular.module('app', ['ngRoute']);
 
 app.config(function ($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl: 'read.html',
-        controller: 'Index'
-    })
-});
-
-app.controller('Index', Index);
-
-function Index($scope, $http) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'read.html',
+            controller: 'Index'
+        })
+        .when('/create', {
+            templateUrl: 'create.html',
+            controller: 'create'
+        })
+}).controller('Index', ['$scope', '$http', 'person', function Index($scope, $http, Person) {
+    debugger;
 
     $http.get('json/person.json')
         .then(function (res) {
-            $scope.persons = res.data;
+
+            Person.newData(res.data);
+            $scope.persons = Person.get();
         });
 
+    $scope.submit = function (Person) {
+        $("#createPerson").modal('toggle');
+        $scope.persons.push(Person);
+        $scope.Person = {};
+    }
 
-}
+    function reset() {
+
+    }
+}]).controller('create', ['$scope', 'person', function Create($scope, person) {
+    $scope.Person = {};
+    debugger;
+    person.set({
+        "firstname": "Tate",
+        "lastname": "Imani",
+        "age": 64,
+        "company": "Urna Associates"
+    });
+
+}]).factory('person', [function person() {
+
+    var Persons = new Array();
+
+    function getPerson() {
+        return Persons;
+    }
+    return {
+        get: getPerson,
+        set: function (data) {
+            Persons.push(data);
+        },
+        newData: function (data) {
+            debugger;
+            Persons = Persons.length == 0 ? data : Persons;
+        }
+    }
+
+    }]);
+
+
+
+
+
+
+app.directive('createPerson', function () {
+    var controller = ['$scope', function ($scope) {
+
+
+        function init() {
+            $scope.persons = $scope.datasource;
+        }
+        init();
+
+        $scope.submit = function (Person) {
+            debugger;
+            //init();
+            $("#createPerson").modal('toggle');
+            $scope.persons.push(Person);
+            $scope.Person = {};
+        }
+
+    }];
+    return {
+        scope: {
+            datasource: '=',
+            add: '&',
+        },
+        controller: controller,
+        templateUrl: '_create.html'
+
+
+    }
+});
